58edf59d010dece188eb4debbf2c0eb0216de161
ahmedhasan689 <ahm19edhasan@gmail.com>
2025-05-13 13:41:39 +0300

    App Layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <app-layout>
+    <router-view />
+  </app-layout>
 </template>
+
+<script>
+import AppLayout from "@/components/global/AppLayout.vue";
+
+export default {
+  components: {
+    AppLayout,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/global/AppLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout">
+    <v-layout>
+      <!-- Start::Cart Drawer Component -->
+      <CartDrawer />
+      <!-- End::Cart Drawer Component -->
+      <!-- Start::Navigation Component -->
+      <AppNavigation />
+      <!-- End::Navigation Component -->
+      <v-main>
+        <slot></slot>
+      </v-main>
+      <!-- Start::Footer Component -->
+      <AppFooter />
+      <!-- End::Footer Component -->
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import AppNavigation from "@/components/global/AppNavigation.vue";
+import AppFooter from "@/components/global/AppFooter.vue";
+import CartDrawer from "@/components/global/CartDrawer.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    AppNavigation,
+    AppFooter,
+    CartDrawer,
+  },
+};
+</script>
